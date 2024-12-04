4e41456a73efcc0affca2faf6b3fe275b39a212e
Ignacio Cuevas <icuevas@seenka.com>
2024-12-04 13:50:19 +0100

    Adjust parameters for ML-KEM1024

--- code/jasmin/mlkem_ref/params.h
+++ code/jasmin/mlkem_ref/params.h
@@ -2,7 +2,7 @@
 #define PARAMS_H
 
 #ifndef MLKEM_K
-#define MLKEM_K 3 /* Change this for different security strengths */
+#define MLKEM_K 4 /* Change this for different security strengths */
 #endif
 
 /* Don't change parameters below this line */
