d370ec85110825a700bc96cb318a713b8fed4b05
Ignacio Cuevas <icuevas@seenka.com>
2024-12-06 15:25:02 +0100

    Fix poly.jinc
    i in compress functions must iterate to MLKEM_N and was failing because of the test

--- code/jasmin/mlkem_ref/test/test_poly_compress.c
+++ code/jasmin/mlkem_ref/test/test_poly_compress.c
@@ -13,8 +13,8 @@ int main(void)
 {
   int test_ok = 1;
   size_t test_iteration = 0;
-  unsigned char out0[128];
-  unsigned char out1[128];
+  unsigned char out0[160]; // MLKEM_K = 4
+  unsigned char out1[160]; // MLKEM_K = 4
   poly a;
   
   while(test_ok == 1 && test_iteration < TEST_ITERATIONS)
@@ -24,7 +24,7 @@ int main(void)
     poly_compress(out0, &a);
     poly_compress_jazz(out1, &a);
 
-    for(int i=0;i<128;i++)
+    for(int i=0;i<160;i++)
     { if(out0[i] != out1[i])
       { fprintf(stderr, "ERROR: poly_compress: %d, %d, %d\n", i, out0[i], out1[i]);
         test_ok = 0;
